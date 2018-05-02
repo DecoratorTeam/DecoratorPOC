55b8ced3a56f5b954749993c39722ebefa9a8729
Himaja <Himaja@LMINHYDTST0007>
2018-05-02 14:33:25 +0530

    first commit

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ var mongoose = require('mongoose');
 const express = require("express");
 const bodyParser = require('body-parser');
 const app = express();
-//const port = 3000;
 const port = process.env.PORT || 3000;
 
 var userservice = require('./Services/UserService');
@@ -21,6 +20,10 @@ app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
 	res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
     next();
+});
+
+app.get('/', function(req, res) {
+  res.sendFile('index.html', {root: __dirname })
 });
 
 mongoose.connect("mongodb://sravani:sravani123@52.171.192.111:27017/decorator_db_poc");
